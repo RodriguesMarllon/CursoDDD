92214c2fa20c590afb271ee6e9e24ff628bc4a97
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-12-01 19:59:21 -0300

    aula 015 - Associações

---
+++ Churras/Empresa.Churras.Domain.Model/Entities/EventoColegaConfirmado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empresa.Churras.Domain.Model.Entities
+{
+    public class EventoColegaConfirmado
+    {
+        public long ColegaKey { get; set; }
+        public string ColegaNome { get; set; }
+        public string OQueVaiLevar { get; set; }
+    }
+}
